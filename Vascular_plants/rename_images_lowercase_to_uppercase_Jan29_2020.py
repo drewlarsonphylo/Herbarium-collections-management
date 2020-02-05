63e91302975ed8fc6621c3beac6f48ea2c8175b0
Drew <drew@Drews-MacBook-Pro.local>
2020-02-05 11:03:25 -0500

    reorganization

---
+++ Vascular_plants/rename_images_lowercase_to_uppercase_Jan29_2020.py
@@ -0,0 +1,46 @@
+#Chages file names that are *_e* to *_E* and *_g* to *_G*
+
+only_test=False #When this is set to True, this script will print a statement for all the things would move, but will not actually change any file names
+output_file="renamed_files.csv" #Name of the outout csv it makes with the old and new file names
+
+import os,re
+
+#Set the value for the base dir
+basedir=os.getcwd()
+#Checking os_type based on os.getcwd() and automatically setting it
+if "\\" in basedir: #This is indicitive of Windows path names
+	print "Using Windows path names"
+	slash="\\"
+elif "/" in basedir: #This is indicitive of Linux or Mac
+	print "Using Mac path names"
+	os_type="mac"
+	slash="/"
+
+#Generate a list of all the image dirs in the main dir
+allfiles=os.listdir(".")#Generates a list of all files in the main dir
+Image_dirs={}#Initalize a dict to store names of image dirs
+for j in allfiles:#Parse through the list and find which are image dirs
+	if len(re.findall("[0-9]{7}\ -\ [0-9]{7}",j))==1:#Check if the item has 7 digits followed by space - space and 7 more digits -- the format of the image dirs.
+		Image_dirs[j]=j#If it has the correct format, add the dir name to the dict that stores the names of the image dirs
+
+
+#Go through each folder and rename files accordingly, print a record of what it does
+with open(output_file,"w+") as out:
+	for folder in Image_dirs:
+		for fil in os.listdir(folder):
+			
+			###Additional things to replace can be added with a new .replace statement here
+			newname=fil.replace("_g","_G").replace("_e","_E")
+			#############################################################################
+			
+			if fil != newname: #These aren't the same if the file needs to be renamed
+				if only_test!=True:
+					print "renaming",folder+slash+fil,"to",folder+slash+newname #Printing what is happening to the screen
+					os.rename(folder+slash+fil,folder+slash+newname) #Actually doing the rename
+					out.write(folder+slash+fil+","+folder+slash+newname+"\n") #Writing the old and new file name to the output file
+					
+				elif only_test == True:
+					print "will rename",folder+slash+fil,"to",folder+slash+newname #Printing what would have been moved to the screen
+					
+				
+	
---
+++ Vascular_plants/verify_existance_for_list_specimen_images_option_to_copy_Feb4_2020.py
@@ -0,0 +1,233 @@
+#The script takes a Target list, parses through the herbarium directory structure, and reports whether there are images for the requested specimens.
+#It reports the number of hits and the file paths for each hit
+
+
+#In the Feb4 version, the target list can be either a list of 7 digit barcodes in which case hits will be any file in the archive with that barcode
+#In the Feb4 version, the target list can also be a list of complete file names you are searching for, in which case hits will be only files in archives with that exact name
+
+#Run from the base directory with all the image archive folders in it
+
+#Run like this:      /path/to/python2.7 /path/to/verify_specimen_images.py
+
+#Importing the necessary modules to run this script
+import re,os,shutil
+
+
+#Hardcode things|| Changed by user as needed
+inputTargetPath="TargetList.txt" #The input file with a list of UMICH Image file names to search for.
+# Examples like MICH-V-1234567_1.dng MICH-V-7654321B.CR2 --- Should be in the folder from which this script is run
+
+output_filename="output.csv" #The name of the file that this script will output    >>>Warning, any file with this name will be overwritten by this script<<<
+
+header=True #Change True or False depending on if you want a header line on the output file specifying the columns
+
+include_full_filepath_names=True #True/False if you would like full file path names for any hits included in the output. If False, only the folder the image is in will be included
+
+copy_image_hits=False #If True, all images that hit to one of the target barcodes will be copied to the folder specified below in "copy_image_directory" 
+#^THIS MAY TAKE A LONG TIME FOR LARGE NUMBERS OF FILES
+
+copy_image_directory="Target_images" #Specify what you want the folder to be called that all the images will be copied to if copy_image_hits is set to True
+
+
+#Defining a function that checks if an image name is legitimate. It takes a string (the file name) and returns True or False
+def verify_name_legitimate(filename):
+	import re
+	acceptable_codes=["E","G","T","P","S"]
+	if " " in filename: #This are all the criteria that we are checking for that cause the name to fail. Starting with if it has a space
+		return False
+	elif ")" in filename: #There can't be any parathenses is the file name
+		return False
+	elif "(" in filename: #There can't be any parathenses is the file name
+		return False
+	elif len(re.findall("(^[0-9]{7})",filename.strip("MICH-V-"))) !=1: #Fail if there isn't at least 7 consecutive numbers at the start of the file name with or without MICH-V-
+		return False
+	elif len(re.findall("[0-9]{8}",filename)) ==1: #Fail if there are 8 (or more) consecutive numbers at any point
+		return False
+	elif len(re.findall("\.",filename))>1: #Fail if there are multiple periods in the file name
+		return False
+	elif re.findall("[0-9]{7}[A-Z]?_?[0-9GETPS]?\.[a-zCR2]+",filename.strip("MICH-V-")) != [filename.strip("MICH-V-")]: #Fail if what follows the 7 digit barcode isn't a legitimate UMICH code. Add new accepted suffixes here
+		# ^This could fail things that are acceptable when there are two underscores, need handle things smarter
+		#At this point the files barcode has 7 digits
+		postcode=filename.strip("MICH-V-").split(".")[0][7:] #This gives a string with only whats between the barcode and the period
+		#In the rare case were there is a number immediately following an underscore and an acceptable letter variation, don't make it fail	
+		#MICH-V-1234567A_E_G_1.dng
+		splits=postcode.split("_")
+		s_dict={}
+		place=0
+		has_num=False
+		#The only things that should end up here fail the re above, so have multiple underscores. Therefore, the first either needs to be A-Z or empty always
+		#If there is a number, it has to come last. There shouldn't ever be two Gs or two Es or two Ts
+		for item in splits:
+			if item in s_dict: #This means there is a repeated letter or number in postcode, which shouldn't happen. So this fails
+				return False
+			else:
+				s_dict[item]=place #If it's not duplicated, then store in dict with value as the position, and key as the codeletter
+				#Check if it's a number and flip on the switch if it is
+				if item.isdigit() == True:
+					if has_num==False:
+						has_num=item
+					elif has_num!=False: #This means there is more than one number, which is a criteria for failure
+						return False
+				place+=1
+		#Now go through all the items in s_dict and check if they are in valid positions compared to their letters
+		if "" in s_dict:
+			if s_dict[""]!=0: #So if there is an empty underscore but it's not first
+				return False
+		elif "" not in s_dict: #This means there is no empty underscore, so there must be exactly one A-Z in position 0:
+			if len(re.findall("[A-Z]",splits[0]))!=1:
+				return False					
+		if has_num!=False: #If there is a number, it has to come in the last position. There can only be one or it's already failed.
+			if s_dict[has_num]!=len(s_dict)-1:
+				return False
+		#Now check that all the codes not otherwise checked are valid. There is a narrow subset of letters allowable for positions other than splits[0]. If there is a number we know
+		#that it's last now and don't have to account for that.
+		if has_num!=False: #This means there is a number in the last position, so don't check last position
+			tocheck=splits[1:-1]
+		elif has_num==False: #Also have to check the final position
+			tocheck=splits[1:]
+		for code in tocheck:
+			if code not in acceptable_codes:
+				return False	
+		return True	#If the name has made it all the way through to this point in the double underscore case it is acceptable	
+	else: #If the file name doesn't meet any of the criteria, don't flag it as bad
+		return True
+
+
+
+
+
+
+
+
+###MAIN FUNCTION###
+if __name__ == "__main__":
+	
+	#Making sure the output file and error file are empty before starting the script
+	if output_filename in os.listdir("."):
+		os.remove(output_filename)
+	if "errors.txt" in os.listdir("."):
+		os.remove("errors.txt")
+		
+	#Set the value for the base dir
+	basedir=os.getcwd()
+	
+	#Checking os_type based on os.getcwd() and automatically setting it
+	if "\\" in basedir: #This is indicitive of Windows path names
+		print "Using Windows path names"
+		os_type="windows"
+	elif "/" in basedir: #This is indicitive of Linux  or Mac
+		print "Using Mac path names"
+		os_type="mac"
+	
+	#Making sure the copy_image_directory exists if needed	
+	if copy_image_hits==True:
+		if copy_image_directory not in os.listdir("."):
+			os.makedirs(copy_image_directory)
+	
+	targets={}#initalizing a dir to save all target file  into. Key is the file; the value is a list of all image file paths associated with the file name
+	#Reading in the target file and saving them in a dir
+	with open(inputTargetPath,"r") as tar:
+		for line in tar:
+			fil=line.strip()
+			
+			#Checking if the file name is formatted correctly 
+			if verify_name_legitimate(fil)==False:
+				print "Error, target file contains illegitimate file name. Writing the offending file name to 'errors.txt':",fil
+				with open("errors.txt","a+") as err:
+						err.write(fil+"\n")
+						
+			elif verify_name_legitimate(fil)==True:#If it's a valid file name save the target in the dictionary
+				targets[fil]=fil
+		
+		
+		
+	#Generate a list of all the image dirs in the main dir
+	allfiles=os.listdir(".")#Generates a list of all files in the main dir
+	Image_dirs={}#Initalize a dict to store names of image dirs
+	for j in allfiles:#Parse through the list and find which are image dirs
+		if len(re.findall("[0-9]{7}\ -\ [0-9]{7}",j))==1:#Check if the item has 7 digits followed by space - space and 7 more digits -- the format of the image dirs.
+			Image_dirs[j]=j#If it has the correct format, add the dir name to the dict that stores the names of the image dirs
+	
+	
+	
+	#If user requested a header to the output file, output that here
+	if header==True:
+		head="Target,Match/no match, Number of matches,Filenames\n"
+		print "\n"+head.strip()
+		with open(output_filename,"a+") as out:
+			out.write(head)
+	
+	
+	
+	#For each file target, find which image folder the target file(s) would be in if they exist. There may be two options in some cases because of how the images are currently stored
+	#Compare the first three digits of the target file barcode and look through image directories with the same three first digits
+	for x in targets:#Iterates though all the targets
+		match=0#Default to no match, this gets switched on if a match if found
+		num_matches=0#Default to no matches
+		list_of_output_for_target={}
+		i=x.strip("MICH-V-")
+		
+		for item in Image_dirs:
+			if i[:3] == item[:3]: #This means the first three digits of the target barcode match the first three of the Image dir 
+			#A match means that this dir needs to be searched for one or more targets
+				for fil in os.listdir(item):
+					if x in fil:
+						match=1
+						num_matches+=1#Notes that an additional match was found
+						
+						
+						#The path for the match will be the Image dir, plus the image file name.
+						if os_type=="mac":
+							if include_full_filepath_names==True: #Based on user specified value
+								output_entry=str(basedir+"/"+item+"/"+fil)
+							else: #User wanted the shorted path names
+								output_entry=str(item+"/"+fil)
+							list_of_output_for_target[output_entry]=output_entry #With either path name length, store the entry to count up later
+							
+						elif os_type=="windows":
+							if include_full_filepath_names==True: #Based on user specified value
+								output_entry=str(basedir+"\\"+item+"\\"+fil)
+							else: #User wanted the shorted path names
+								output_entry=str(item+"\\"+fil)
+							list_of_output_for_target[output_entry]=output_entry #With either path name length, store the entry to count up later
+						
+						else:
+							print 'You appear to have mis-specified os_type, please specify either "mac" or "windows" and try again'
+							exit(1)
+	
+	
+					
+		#If after searching, output lines for each successful search OR there is no match, output that here
+		if match==1:
+			output_line_format=str(x)+","+str("match")+","+str(num_matches)#Initialize the output line to add file paths to
+			for foundTarget in list_of_output_for_target:
+				output_line_format+=","+str(foundTarget)
+				
+				
+				
+				#If copy_image_hits is set as True, copy the file into the directory
+				if copy_image_hits==True:
+					shutil.copy2(foundTarget,copy_image_directory)#This command actually does the copy of the image, copy2 preserves metadata
+				
+				
+				
+			output_line_format+="\n"#Add a tailing newline after all the paths have been added
+			print output_line_format.strip()#not necessary
+			
+			with open (output_filename,"a+") as out:#Opens the output file to append more data
+					out.write(output_line_format)			
+			
+	
+	
+		if match==0:
+			output_line_format=str(x)+","+str("no match")+","+str(num_matches)+"\n"
+			print output_line_format.strip()#not necessary
+			with open (output_filename,"a+") as out:#Opens the output file to append more data
+					out.write(output_line_format)
+	
+		
+	print "\nscript completed"
+	print "A csv of target hits was printed to",output_filename
+	if copy_image_hits==True:
+		print "Images of target hits were copied to the folder",copy_image_directory
+	
